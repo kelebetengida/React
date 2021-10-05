8bb2a7260c01d9db3f6e789057521ae7f5b14390
Kelebet Engida <engida398@gmail.com>
2021-10-05 13:25:19 -0700

    added new components with their styles.

--- my-app/src/components/PageProps/index.js
+++ my-app/src/components/PageProps/index.js
@@ -1,6 +1,11 @@
-import React from "react";
 
-const PageContent=(props)=>{
+import React, { useState } from 'react';
+
+export default function PageContent(){
+    const [greeting, setPageContent] = useState('Welcome! React state is awesome!');
+
+}
+const pageContent=(props)=>{
     return(
         <div>
             {props.children}

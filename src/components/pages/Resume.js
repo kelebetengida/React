9f55145cba1dda4fee4aee21a52a1bed4e207395
Kelebet Engida <engida398@gmail.com>
2021-10-29 21:53:13 -0700

    A newly redesign contact me page.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,14 +1,47 @@
 import React from "react";
-import img from "../Assets/Picture1.png"
-const Resume = () => {
+import RESUME from "../Assets/RESUME.pdf"
+
+export default function Resume() {
     return (
-        <div style={{ background: "linear-gradient(360deg, #AFD275 9%, #C2CAD0 100%, #AFD275 9%)" ,justifyContent: "center"}}>
-            <h1 style={{ display: "flex", justifyContent: "center" }}>Resume</h1>
+        <div style={{ background: "linear-gradient(360deg, #AFD275 9%, #C2CAD0 100%, #AFD275 9%)", justifyContent: "center" }}>
+            <main>
+                <section 
+                sstyle={{ margin: "auto",
+                    width: "50%",
+                    border: "3px solid green",
+                    padding: "10px", }}>
+                    <h1>Resume</h1>
+                    <div class="col-md-6">
+                        <a href={RESUME}>Click Here To Download My Resume</a>
+                        <h4>
+                            Front-end Proficiencies
+                        </h4>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>jQuery</li>
+                            <li>Responsive Design</li>
+                            <li>React</li>
+                            <li>Bootstrap</li>
+                        </ul>
 
-            <img style={{ width: "60%",  alignItems:"center", marginLeft:"300px"}} src={img}alt="" />
+                        
+                        <h4>
+                            Back-end Proficiencies
+                        </h4>
+                        <ul>
+                            <li>APIs</li>
+                            <li>Node</li>
+                            <li>Express</li>
+                            <li>MySQL, Sequelize</li>
+                            <li>MongoDB, Mongoose</li>
+                            <li>REST</li>
+                            <li>GraphQL</li>
+                        </ul>
+                    </div>
+                </section>
+            </main>
         </div>
-    );
-
-};
-
-export default Resume;
+    )
+}
